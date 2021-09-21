41dd869a354288548b4d8f044d90780b71ca257a
Liuizn <Luizgabrielsnn@gmail.com>
2021-09-20 21:42:50 -0300

    add css in procedimentos

--- cadastro-proc.php
+++ cadastro-proc.php
@@ -1,31 +1,50 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="size-inputs.css">
+</head>
+
 <body>
-    <h2>Insira as informações do procedimento</h2>
-    <form action="insert-proc.php" method="post">
+    <h1>Insira as informações do procedimento</h1>
+    <form action="insert-proc.php" method="post" class="pai-area-formulario">
+        <div class="group-inputs nome">
+            <div class="group" id="nome">
+                <label for="nome_procedimento">Nome Procedimento</label>
+                <input type="text" name="nome_procedimento" id="nome_procedimento" maxlength="50" required>
+            </div>
 
-        <label for="nome_procedimento">Nome:</label>
-        <input type="text" name="nome_procedimento" id="nome_procedimento" maxlength="50" required><br><br>
+            <div class="group">
+                <p id="data">Data de registro</p>
+            </div>
 
-        <label for="valor_procedimento">Valor:</label>
-        <input type="text" name="valor_procedimento" id="valor_procedimento" maxlength="60" required><br><br>
+        </div>
 
-        <label for="genero">Genero:</label>
-        <select name="genero" id="genero">
-            <option value="Ambos">Ambos</option>
-            <option value="Feminino">Feminino</option>
-            <option value="Masculino">Masculino</option>
-        </select> <br> <br>
+        <div class="group-inputs">
+            <div class="group">
+                <label for="valor_procedimento">Valor</label>
+                <input type="text" name="valor_procedimento" id="valor_procedimento" maxlength="60" required>
+            </div>
+            <div class="group">
+                <label for="genero">Genero:</label>
+                <select name="genero" id="genero">
+                    <option value="Ambos">Ambos</option>
+                    <option value="Feminino">Feminino</option>
+                    <option value="Masculino">Masculino</option>
+                </select>
+            </div>
+        </div>
 
-        <p id="data">Data de registro</p>
 
-        <button><a href="lista-proc.php">Procedimentos cadastrados
-            </a></button>
-        <style>
-            A {
-                text-decoration: none;
-                color: black
-            }
-        </style>
-        <input type="submit" value="Cadastrar">
+
+
+        <div class="group-inputs group-buttons">
+            <button id="b_medicos"><a href="lista-proc.php">Ver Procedimentos</a></button>
+            <input type="submit" value="Cadastrar" id="b_cadastrar">
+            <input type="reset" value="Limpar" id="b_limpar">
+        </div>
+
+
+
+
 
     </form>
 
