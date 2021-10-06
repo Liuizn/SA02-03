76728f0668d5edea018442e9cd58ff9484c671a9
Liuizn <Luizgabrielsnn@gmail.com>
2021-10-06 14:17:17 -0300

    create new branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# SA02
-Repositório com a função de armazenar a Situação de Aprendizagem
--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $con = new PDO("mysql:host=localhost;dbname=agencia_estetica","root","");
-    
-}catch(PDOException $e){
-    echo "Erro ao acessar banco". $e->getMessage();
-};
-
-?>
