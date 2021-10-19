b873be5cef092c4e7eac688ad72b3badf229c866
Luiz Gabriel Dos Santos Nogueira <luizgabrielsnn@gmail.com>
2021-10-19 13:25:42 -0300

    upload

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="style-index.css">
-</head>
 
 <body>
     <div class="container-grid-index">
@@ -48,5 +42,3 @@
 </script>
 
 </body>
-
-</html>
